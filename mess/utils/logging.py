6e25cf3360316b8c8be93dbd7a4e9ef45d5d3d74
Tom Magee <tom@mariposa.coop>
2012-09-12 16:06:13 -0400

    Bug update to log function. See comment at top of function

--- mess/utils/logging.py
+++ mess/utils/logging.py
@@ -8,22 +8,55 @@ FLAG_MAP = {
     'delete': DELETION,
 }
 
+# logs any action on a database record to django's admin interface
+#
+# HOW IT WORKS: i
+# - If the caller supplied a change_message, that is the 
+#   only entry logged.
+# - If the record is new or deleted, the entry simply states as much
+# - If the record has been updated, this function cycles through the 
+#   record's old values and logs a message for each value that was changed
 def log(request, obj, action_flag, change_message='', old_values=None):
-    if old_values:
-        change_message_list = []
-        #raise Exception, old_values['hours_balance']
+
+    if change_message:
+	    LogEntry.objects.log_action(
+	        user_id = request.user.pk,
+	        content_type_id = ContentType.objects.get_for_model(obj).pk,
+	        object_id = obj.pk,
+	        object_repr = obj._meta.app_label + "." + obj._meta.object_name,
+	        action_flag = FLAG_MAP[action_flag],
+	        change_message = change_message,
+	    )
+    elif FLAG_MAP[action_flag] == ADDITION:
+	    LogEntry.objects.log_action(
+	        user_id = request.user.pk,
+	        content_type_id = ContentType.objects.get_for_model(obj).pk,
+	        object_id = obj.pk,
+	        object_repr = obj._meta.app_label + "." + obj._meta.object_name,
+	        action_flag = FLAG_MAP[action_flag],
+	        change_message = "New record added"
+	    )
+    elif old_values and FLAG_MAP[action_flag] == CHANGE:
         for key in old_values:
             if not key.startswith('_') and \
                     old_values[key] != obj.__dict__[key]:
-                change_message_list.append('Changed %s: "%s" to "%s"' % 
-                        (key, old_values[key], obj.__dict__[key]))
-        change_message = '\n'.join(change_message_list)
-    LogEntry.objects.log_action(
-        user_id = request.user.pk,
-        content_type_id = ContentType.objects.get_for_model(obj).pk,
-        object_id = obj.pk,
-        object_repr = force_unicode(obj),
-        action_flag = FLAG_MAP[action_flag],
-        change_message = change_message,
-    )
 
+                change_message = 'Changed %s: "%s" to "%s"' % (key, old_values[key], obj.__dict__[key])
+	            
+                LogEntry.objects.log_action(
+	                user_id = request.user.pk,
+	                content_type_id = ContentType.objects.get_for_model(obj).pk,
+	                object_id = obj.pk,
+	                object_repr = obj._meta.app_label + "." + obj._meta.object_name,
+	                action_flag = FLAG_MAP[action_flag],
+	                change_message = change_message,
+	            )
+    elif FLAG_MAP[action_flag] == DELETION:
+	    LogEntry.objects.log_action(
+	        user_id = request.user.pk,
+	        content_type_id = ContentType.objects.get_for_model(obj).pk,
+	        object_id = obj.pk,
+	        object_repr = obj._meta.app_label + "." + obj._meta.object_name,
+	        action_flag = FLAG_MAP[action_flag],
+	        change_message = "Record deleted"
+	    )
