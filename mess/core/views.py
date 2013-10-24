d4324d8130aafa8c5a8aeb904339566a8f48dad2
Tom Magee <tom@mariposa.coop>
2013-10-24 16:15:19 -0400

    Fixed bug introduced by commit f381b5f9220eebb314f13f580ce441dbefa573
    The commit introduced a bug that caused MESS to crash when user
    attempted to login with bad ID

--- mess/core/views.py
+++ mess/core/views.py
@@ -35,34 +35,36 @@ def welcome(request):
         redirect = request.POST.get('next', reverse('welcome'))
         #raise Exception, auth_form.cleaned_data
         return HttpResponseRedirect(redirect)
-    else:
-
-      if request.user.is_authenticated():
-        entries = cache.get('entries')
-        if not entries:
-            socket.setdefaulttimeout(TIMEOUT)
-            feed = feedparser.parse("http://www.mariposa.coop/?feed=rss2")
-            socket.setdefaulttimeout(NORMAL_TIMEOUT)
-            entries = feed.entries[:MAX_ENTRIES]
-            cache.set('entries', entries, 300)
-        context['rss_entries'] = entries
-
-        # not sure why we're getting the FieldDoesNotExist error with locmem
-        # but this try/except block seems to handle it
-        try:
-            threads = cache.get('threads')
-        except FieldDoesNotExist:
-            threads = None
-        if not threads:
-            threads = f_models.Post.objects.threads()[:MAX_ENTRIES]
-            cache.set('threads', threads)
-        context['threads'] = threads
-        context['member'] = m_models.Member.objects.get(user=request.user)
-        template = get_template('welcome.html')
       else:
-        auth_form = auth_forms.AuthenticationForm()
-        context['next'] = request.GET.get('next', '')
         context['form'] = auth_form
+        context['next'] = request.GET.get('next', '')
         template = get_template('welcome-anon.html')
+    elif request.user.is_authenticated():
+      entries = cache.get('entries')
+      if not entries:
+          socket.setdefaulttimeout(TIMEOUT)
+          feed = feedparser.parse("http://www.mariposa.coop/?feed=rss2")
+          socket.setdefaulttimeout(NORMAL_TIMEOUT)
+          entries = feed.entries[:MAX_ENTRIES]
+          cache.set('entries', entries, 300)
+      context['rss_entries'] = entries
+
+      # not sure why we're getting the FieldDoesNotExist error with locmem
+      # but this try/except block seems to handle it
+      try:
+          threads = cache.get('threads')
+      except FieldDoesNotExist:
+          threads = None
+      if not threads:
+          threads = f_models.Post.objects.threads()[:MAX_ENTRIES]
+          cache.set('threads', threads)
+      context['threads'] = threads
+      context['member'] = m_models.Member.objects.get(user=request.user)
+      template = get_template('welcome.html')
+    else:
+      auth_form = auth_forms.AuthenticationForm()
+      context['form'] = auth_form
+      context['next'] = request.GET.get('next', '')
+      template = get_template('welcome-anon.html')
 
     return HttpResponse(template.render(context))
