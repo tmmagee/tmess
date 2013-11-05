20ae0d87a95b7ce2047930dc250fe3a12a6c2be0
Tom Magee <tom@mariposa.coop>
2013-11-04 20:22:48 -0500

    Fixes for bugs noticed by Mike's clean install of MESS

--- mess/revision/__init__.py
+++ mess/revision/__init__.py
@@ -15,14 +15,14 @@ def account_revision_has_changes(revision):
   return revision != most_recent_revision
 
 @receiver(post_save, sender=m_models.Member)
-def record_member_revision(signal, sender, instance, created, raw, using):
+def record_member_revision(signal, sender, instance, created, raw, using, **kwargs):
   member_revision = models.MemberRevision.create_member_revision(member=instance)
 
   if not created and not member_revision_has_changes(member_revision):
     member_revision.delete()
 
 @receiver(post_save, sender=m_models.Account)
-def record_account_revision(signal, sender, instance, created, raw, using):
+def record_account_revision(signal, sender, instance, created, raw, using, **kwargs):
 
   account_revision = models.AccountRevision.create_account_revision(account=instance)
 
