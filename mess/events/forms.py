b606ba4fb56412046c95527a06d26f40d9a71147
Tom Magee <tom@mariposa.coop>
2014-01-28 20:45:34 -0500

    Added autocomplete to orientation form

--- mess/events/forms.py
+++ mess/events/forms.py
@@ -1,10 +1,34 @@
+from django import forms
 from django.forms import ModelForm
 from django.contrib.admin import widgets
 from mess.events.models import Orientation, Location
+from mess.membership import models as m_models
+from mess.autocomplete import AutoCompleteWidget
 
 class OrientationForm(ModelForm):
     class Meta:
         model = Orientation
+
+    staff_contact = forms.ModelChoiceField(m_models.Member.objects.all(),
+      widget=AutoCompleteWidget('member_spiffy',
+        view_name='membership-autocomplete', 
+        canroundtrip=True),
+      required=False, 
+      help_text='* = include inactive') 
+
+    facilitator = forms.ModelChoiceField(m_models.Member.objects.all(),
+      widget=AutoCompleteWidget('member_spiffy',
+        view_name='membership-autocomplete', 
+        canroundtrip=True),
+      required=False, 
+      help_text='* = include inactive') 
+
+    cofacilitator = forms.ModelChoiceField(m_models.Member.objects.all(),
+      widget=AutoCompleteWidget('member_spiffy',
+        view_name='membership-autocomplete', 
+        canroundtrip=True),
+      required=False, 
+      help_text='* = include inactive') 
 
     def  __init__(self, *args, **kwargs):
         super(OrientationForm, self).__init__(*args, **kwargs)
