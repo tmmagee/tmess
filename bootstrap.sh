17fcf68bb5db8e9605ecc4c8bda97a15e705ddda
Gabriel Farrell <g@grrawr.com>
2014-02-10 10:59:32 -0500

    Vagrant up and running
    This gets us as far as the "unable to open database file" screen.
    Still need to set up the local_settings.py and see if the messdb.sh
    script can indeed load the DB. Refs #440

---
+++ bootstrap.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y gunicorn nginx postgresql python-dateutil python-feedparser
+
+# Install Django
+cd /tmp
+wget https://www.djangoproject.com/m/releases/1.4/Django-1.4.10.tar.gz
+tar xzvf Django-1.4.10.tar.gz
+cd Django-1.4.10
+python setup.py install
+cd ..
+rm -rf Django-1.4.10*
+
+# Configure nginx
+cat > /etc/nginx/sites-available/mess << 'EOF'
+server {
+  listen 80;
+  server_name mess.mariposa.coop;
+  client_max_body_size 10m;
+  client_body_buffer_size 128k;
+    
+  keepalive_timeout 1000;
+
+  location /media {
+    alias /vagrant/mess/media;
+  }
+
+  location / {
+      # an HTTP header important enough to have its own Wikipedia entry:
+      #   http://en.wikipedia.org/wiki/X-Forwarded-For
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+      # pass the Host: header from the client right along so redirects
+      # can be set properly within the Rack application
+      proxy_set_header Host $host;
+
+      # we don't want nginx trying to do something clever with
+      # redirects, we set the Host: header above already.
+      proxy_redirect off;
+
+      proxy_pass http://127.0.0.1:8000;
+  }
+}
+EOF
+/etc/init.d/nginx restart
+
+# Configure gunicorn
+cat > /etc/gunicorn.d/mess << 'EOF'
+CONFIG = {
+  'mode': 'django',
+  'working_dir': '/vagrant/mess/',
+  'user': 'www-data',
+  'group': 'www-data',
+  'args': (
+    '--bind=127.0.0.1:8000',
+    '--worker-class=egg:gunicorn#sync',
+    '--timeout=1000',
+    # For dev. See http://stackoverflow.com/questions/12773763/gunicorn-autoreload-on-source-change
+    '--max-requests=1',
+    '--workers=1',
+  ),
+}
+EOF
+/etc/init.d/gunicorn restart
