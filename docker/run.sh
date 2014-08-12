2086b42ce1339ed67018c666cf427e434296d342
Gabriel Farrell <g@grrawr.com>
2014-08-11 20:18:44 -0400

    Docker containers will be built elsewhere

--- docker/run.sh
+++
@@ -1,4 +0,0 @@
-#docker run -P -v $PWD/mess:/usr/local/src/mess -t gsf747/mess gunicorn mess.wsgi:application -b 0.0.0.0.:8000 --max-requests 1
-#docker run -i -P -v $PWD/mess:/usr/local/src/mess -t gsf747/mess bash
-docker run -name=postgresql -d -v /tmp/postgresql:/data -t gsf747/postgresql
-docker run -name=mess -d -P -link postgresql:db -v $PWD/mess:/usr/local/src/mess -t gsf747/mess
